eb552f66524e1f4181cee4bb697d43cff318fc39
atishpanday <atishpanday1@gmail.com>
2021-04-06 23:56:09 +0530

    updated readme file

--- client/src/components/Benefits.js
+++ client/src/components/Benefits.js
@@ -30,7 +30,7 @@ const Benefits = () => {
         <div className="benefits-container">
             <div className="benefit-card">
                 <div className="benefit-heading-container">
-                    <h1 className="benefit-heading">"Easy booking anytime, anywhere"</h1>
+                    <h1 className="benefit-heading">Easy booking anytime, anywhere</h1>
                 </div>
                 <div className="benefit-text-container">
                     <p className="benefit-text">"You can book an appointment with our doctors anytime through our user-friendly website. We will pre-allocate a time slot for you so that you never have to wait in line"</p>
